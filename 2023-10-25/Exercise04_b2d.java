b158e7e7ba0ee8b885f9b8a08d71ce654e549997
F H <fhuang.uk@gmail.com>
2023-10-21 15:18:04 +0100

    Day4 Exercise 04

--- 2023-10-25/Exercise04_b2d.java
+++ 2023-10-25/Exercise04_b2d.java
@@ -1,18 +1,35 @@
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.*;
 
 public class Exercise04_b2d {
     public static void main(String[] args) {
 
         java.util.Scanner scan = new java.util.Scanner(System.in);
-        System.out.println("What's your binary number: ");
-        String userInput = scan.next();
 
-        Binary2Dec obj = new Binary2Dec();
-        String output = obj.convert(userInput);
-        System.out.println("The decimal number is: ");
-        System.out.println(output);
+        System.out.print("Press 1 to convert binary to decimal, press 2 to convert decimal to binary: ");
+        int choice = scan.nextInt();
+
+        if (choice == 1) {
+            System.out.println();
+            Binary2Dec obj = new Binary2Dec();
+            System.out.print("Enter your binary number: ");
+            String binaryInput = scan.next();
+            String result = obj.convert(binaryInput);
+            System.out.println("Your decimal is: " + result);
+
+        } else if (choice == 2) {
+            System.out.println();
+            DecToBinary obj = new DecToBinary();
+            System.out.print("Enter your decimal number: ");
+            int decimalInput = scan.nextInt();
+            obj.convert(decimalInput);
+        } else {
+            System.out.println("Wrong input, exiting programme.");
+
+        }
 
     }
 }
@@ -55,4 +72,59 @@ class Binary2Dec {
         return result;
     }
 
+}
+
+class DecToBinary {
+
+    public static int[] reverseArrayInPlace(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        return arr;
+    }
+
+    public void convert(int userInput) {
+
+        NearestExponent n = new NearestExponent();
+        double exponent = n.log2(Integer.valueOf(userInput));
+        int arrLength = 0; // determine the length of the binary;
+        if (userInput >= Math.pow(2, exponent)) {
+            arrLength = (int) (exponent + 1);
+        } else {
+            arrLength = (int) (exponent);
+        }
+
+        int[] binaryArr = new int[arrLength];
+
+        for (int i = 0; i < arrLength; i++) { // workout the 2 base order from left to right, so it'll be [1,2,4,8...]
+            binaryArr[i] = (int) (Math.pow(2, i));
+        }
+
+        int[] revserBinaryArr = reverseArrayInPlace(binaryArr); // make the array from right to left [...8,4,2,1]
+        int[] outputBinaryArr = new int[revserBinaryArr.length];
+        double sum = 0;
+        for (int i = 0; i < arrLength; i++) {
+            if (userInput >= revserBinaryArr[i]) {
+                userInput = userInput - revserBinaryArr[i];
+                outputBinaryArr[i] = 1;
+            } else {
+                userInput = userInput - 0;
+                outputBinaryArr[i] = 0;
+            }
+        }
+        System.out.print("The binary number is: ");
+        for (int i = 0; i < outputBinaryArr.length; i++) {
+            System.out.print(outputBinaryArr[i]);
+        }
+
+    }
+}
+
+class NearestExponent { // https://www.geeksforgeeks.org/how-to-calculate-log-base-2-of-an-integer-in-java/
+    public int log2(int N) {
+        int result = (int) (Math.log(N) / Math.log(2));
+        return result;
+    }
 }
